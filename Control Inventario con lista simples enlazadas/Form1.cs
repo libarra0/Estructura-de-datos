3f933e7724ddceb760e14522ba22d84c4e6de741
Leonardo Ibarra Palacios <libarra0@ucol.mx>
2017-04-19 16:00:57 -0500

    Trabajo 2

---
+++ Control Inventario con lista simples enlazadas/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Inventario
+{
+    public partial class Form1 : Form
+    {
+        Inventario Invent;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Invent = new Inventario();
+        }
+
+        private void cmdAgregar_Click(object sender, EventArgs e)
+        {
+            Invent.Agregar(Convert.ToInt32(this.txtCódigo.Text), this.txtNombre.Text,
+                Convert.ToDouble(this.txtPrecio.Text), Convert.ToInt32(this.txtCantidad.Text));
+
+            //borrar la información de cada TextBox
+            txtCódigo.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtCantidad.Text = "";
+        }
+
+        private void cmdEliminar_Click(object sender, EventArgs e)
+        {
+            Invent.Eliminar(Convert.ToInt32(txtCódigo.Text));
+            txtCódigo.Text = "";
+        }
+
+        private void cmdInsertar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdBuscar_Click(object sender, EventArgs e)
+        {
+            txtReporte.Text = ("Código: " +Convert.ToString(Invent.Buscar(Convert.ToInt32(txtCódigo.Text)).Codigo + "\r\nNombre: " + Invent.Buscar(Convert.ToInt32(txtCódigo.Text)).Nombre
+            + "\r\nPrecio: " + Invent.Buscar(Convert.ToInt32(txtCódigo.Text)).Precio + "\r\nCantidad: " + Invent.Buscar(Convert.ToInt32(txtCódigo.Text)).Cantidad));
+            txtCódigo.Text = "";
+        }
+
+        private void cmdReporte_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Control Inventario con lista simples enlazadas/Inventario.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Inventario
+{
+    class Inventario
+    {
+        Producto primero;
+        public Producto raiz;
+
+        public Inventario()
+        {
+            primero = null;
+        }
+        /// <summary>
+        /// Agrega un producto al invetario
+        /// </summary>
+        /// <param name="codigo">Un código</param>
+        /// <param name="nombre">Un nombre</param>
+        /// <param name="precio">Un precio</param>
+        /// <param name="cantidad">Una cantidad</param>
+        public void Agregar(int codigo, String nombre, double precio, int cantidad)
+        {
+            if (primero == null)
+            {
+                primero = new Producto(codigo, nombre, precio, cantidad);
+                raiz = primero;
+            }
+            else
+            {
+                Producto nuevo = new Producto(codigo, nombre, precio, cantidad);
+                primero.setSiguiente(nuevo);
+                primero = nuevo;
+            }
+        }
+        /// <summary>
+        /// Busca un producto en el inventario a partir de un código dado.
+        /// </summary>
+        /// <param name="codigo">Código del producto.</param>
+        /// <returns></returns>
+        public Producto Buscar(int codigo)
+        {
+            Producto puntero = raiz;
+
+            while (puntero.Codigo != codigo)
+            {
+                puntero = puntero._siguiente;
+            }
+            return puntero;
+        }
+
+        public void Eliminar(int codigo)
+        {
+            //Creación de los producto actual y anterior
+            Producto actual, anterior;
+            //Creamos un boleano que nos indique si ya hemos encontrado el producto
+            Boolean encontrado = false;
+            actual = raiz;
+            anterior = null;
+            //Iteración de busqueda
+            while ((actual != null) && (!encontrado))
+            {
+                encontrado = (actual.Codigo == codigo);
+                //Evalua si es verdadero toma el prodcuto actual y el siguiente
+                if (!encontrado)
+                {
+                    anterior = actual;
+                    actual = actual._siguiente;
+                }
+                //Iteración de cambio de enlaces entre productos
+                if (actual != null)
+                {
+                    if (actual == raiz)
+                    {
+                        raiz = actual._siguiente;
+                    }
+                    else
+                    {
+                        anterior._siguiente = actual._siguiente;
+                    }
+                    actual = null;
+                }
+            }
+        }
+    }
+}
+
